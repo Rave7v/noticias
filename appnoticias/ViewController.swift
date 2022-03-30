22b3aa596ef4baad719d2a4d81ec024d62e12151
alexis roman <elsanguinario3257@gmail.com>
2022-03-30 09:50:52 -0600

    Fin de la primera clase

--- appnoticias/ViewController.swift
+++ appnoticias/ViewController.swift
@@ -1,7 +1,20 @@
 
 import UIKit
 
+struct Noticias: Codable {
+    var articles: [Noticia]
+}
+
+struct Noticia: Codable {
+    var title: String?
+    var description: String?
+    //var urlToImage: String
+    //var url: String
+}
+
 class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    //para guardar los articulos y llenar la tabla
+    var noticias = [Noticia]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
@@ -20,8 +33,26 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
         
         tablaNoticias.delegate = self
         tablaNoticias.dataSource = self
+        
+        let urlString = "https://newsapi.org/v2/everything?q=tesla&from=2022-02-28&sortBy=publishedAt&apiKey=888ffbdfb918461a9b071045e1f3ae1f"
+        
+        func analizarJSON(json:Data){
+            let decodificador = JSONDecoder()
+            if let datosDecodificados = try? decodificador.decode(Noticias.self, from: json){
+                print("Los articulos son: \(datosDecodificados.articles.count)")
+                print("Los articulos son: \(datosDecodificados.articles)")
+            }
+        }
+        
+        if let url = URL(string: urlString){
+            if let data = try? Data(contentsOf: url){
+                analizarJSON(json: data)
+            }
+        }
+        
         // Do any additional setup after loading the view.
     }
     
 }
-
+//llave
+//https://newsapi.org/v2/everything?q=tesla&from=2022-02-28&sortBy=publishedAt&apiKey=888ffbdfb918461a9b071045e1f3ae1f
